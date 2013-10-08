c592ba874b51668db91d6ff94429ef154c8c0bd3
robson100 <robson.soares@dce.ufpb.br>
2013-10-08 00:07:35 -0300

    Update GerenteDeInformacao.java

--- CollectiveBuying./src/com/collectivebuying/GerenteDeInformacao.java
+++ CollectiveBuying./src/com/collectivebuying/GerenteDeInformacao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class GerenteDeInformacao {
 	
-	public void imprimirListaClientes(List<Cliente> clientes){
+	public void imprimirListaClientes(List<Cliente> clientes){ //Mini teste 9 (Robson Soares)
 		GerarListaClientes g = new GerarListaClientes(clientes);
 		g.run();
 	}
